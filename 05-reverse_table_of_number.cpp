776f5d98780a0f5e90818776666d29d62ec48cc9
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-08-22 23:29:54 +0500

    task 5

---
+++ 05-reverse_table_of_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"Table of: ";
+    cin>>number;
+    cout<<"======Print Table of "<<number<<" from 1 to 10======"<<endl;
+
+    for(int i=10;i>=1;i--)
+    {
+        cout<<number<<" * "<<i<<" = "<<number*i<<endl;
+    }
+    return 0;
+}
