1c3b23caf1e0344c8f4bd3cdf944bfd431b39a56
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-09-23 09:47:52 +0500

    task 9

---
+++ 09-table_uptill10.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        for(int y=1;y<=10;y++)
+        {
+            cout<<i<<" * "<<y<<" = "<<i*y<<endl;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
